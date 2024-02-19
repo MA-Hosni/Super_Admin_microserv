04bd3b70532610d2b6418f245d11a0b145378059
mohamed ali hosni <mohamedalihosni25@gmail.com>
2024-02-19 10:20:46 +0100

    made sidebar and navbar in the layout for a more clean and scalable code

---
+++ src/app/(layout)/profile/personal/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import React from "react";
+import Sidebar from "@/Components/sidebar/sidebarrr"
+import Navbar from "@/Components/NavBar";
+
+export default function Home() {
+  return (
+    // <div className="container">
+    //     <div className="menu">
+    //         <Sidebar />    
+    //     </div>    
+    //     <div className="content">
+    //         <Navbar />
+    //         <main className="bg-pink-500">
+    //           <h1>personal info page</h1>
+    //         </main>
+    //     </div>
+    // </div>
+    <section>personal info</section>
+    
+  );
+}
