d64c51a3bb1d10f0225ea79efd88721989860444
mohamed ali hosni <mohamedalihosni25@gmail.com>
2024-02-21 20:55:57 +0100

    removed unneeded imports

--- src/app/(layout)/packages/list/page.tsx
+++ src/app/(layout)/packages/list/page.tsx
@@ -1,21 +1,6 @@
-import Image from "next/image";
 import React from "react";
-import Sidebar from "@/Components/sidebar/sidebarrr"
-import Navbar from "@/Components/NavBar";
-
 export default function Home() {
   return (
-    // <div className="container">
-    //     <div className="menu">
-    //         <Sidebar />    
-    //     </div>    
-    //     <div className="content">
-    //         <Navbar />
-    //         <main className="bg-pink-500">
-    //           <h1>packages page</h1>
-    //         </main>
-    //     </div>
-    // </div>
     <section>packages</section>
     
   );
