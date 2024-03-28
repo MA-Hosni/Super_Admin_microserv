a669a8696fa58fa0d56d83b7031cd743674b0dbf
mohamed ali hosni <mohamedalihosni25@gmail.com>
2024-03-28 13:20:30 +0100

    modified tailwind config.ts w 3malt update lel packages lel latest version

--- src/helpers/otpMail.ts
+++ src/helpers/otpMail.ts
@@ -5,7 +5,7 @@ import bcrypt from 'bcrypt';
 
 function generateRandomString(length:any) {
     let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     const charactersLength = characters.length;
     for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
