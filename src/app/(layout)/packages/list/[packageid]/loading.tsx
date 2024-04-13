2f8d63306aa77219ce03edf1985c01a1e3410f5c
mohamed ali hosni <mohamedalihosni25@gmail.com>
2024-04-13 21:53:37 +0100

    zedt loadings

---
+++ src/app/(layout)/packages/list/[packageid]/loading.tsx
@@ -0,0 +1,13 @@
+import Loader from '@/Components/loader'
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <Loader />
+      </div>
+    
+  )
+}
+
+export default Loading
